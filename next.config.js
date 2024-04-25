a103ee81763081e7b3af254b26d4b2697982d271
Cuvii <i@cuvii.dev>
2024-04-25 19:28:59 +0800

    chore: init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["tsx"], // error
+  // pageExtensions:['tsx','ts'] // works
 };
 
 module.exports = nextConfig;
